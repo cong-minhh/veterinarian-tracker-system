c41953986d770f16928edbf241c20a6329b14543
cong-minhh <nguyencongminh722@gmail.com>
2025-06-13 22:28:01 +0700

    Fixed: CRUD and improve UI experience

--- veterinarian-tracker-system/Models/Forms/OwnerFormModel.cs
+++ veterinarian-tracker-system/Models/Forms/OwnerFormModel.cs
@@ -1,34 +1,48 @@
-﻿using System;
+using System;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Http;
+using System.ComponentModel;
 
 namespace TuyetDang.MyVetTracer.ViewModels
 {
     public class OwnerFormModel
     {
-        [Required]
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters")]
+        [RegularExpression(@"^[a-zA-Z0-9_-]+$", ErrorMessage = "Username can only contain letters, numbers, underscores and hyphens")]
+        [DisplayName("Username")]
         public string UserName { get; set; }
 
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        [DisplayName("Email Address")]
         public string Email { get; set; }
 
-        [Phone]
+        [Phone(ErrorMessage = "Please enter a valid phone number")]
+        [DisplayName("Phone Number")]
         public string PhoneNum { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long")]
+        [DisplayName("Password")]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Full name is required")]
+        [StringLength(100, ErrorMessage = "Full name cannot exceed 100 characters")]
+        [DisplayName("Full Name")]
         public string FullName { get; set; }
 
         [DataType(DataType.Date)]
+        [DisplayName("Date of Birth")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime? Dob { get; set; }
 
+        [DisplayName("Gender")]
         public string Gender { get; set; }
 
-        // Upload ảnh
+        [DisplayName("Profile Image")]
+        [DataType(DataType.Upload)]
         public IFormFile ImgFile { get; set; }
     }
 }
